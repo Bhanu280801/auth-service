d2fc604a2d9e94af3dce0ca731f441a4594ed127
Bhanu Prakash <patnambhanuprakash@gmail.com>
2026-01-26 16:34:28 +0530

    Fix root route and cleanup app.js

--- src/app.js
+++ src/app.js
@@ -14,20 +14,19 @@ app.use(express.json())
 app.use(cors())
 app.use(helmet())
 app.use(morgan('dev'))
-app.use(errorHandler)
+
 
 // Swagger Docs Route
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //Routes 
 app.use("/api/auth" , authRoutes)
 
-
+app.use(errorHandler);
 //Basic test route
-app.get('/ ',(req,res)=>{
+app.get('/',(req,res)=>{
     res.send("Auth microservice is running")
 })
 
