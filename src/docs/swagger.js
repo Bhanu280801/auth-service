3f2febb0abd2a713a9823056306c56b8083dd3ce
Bhanu Prakash <patnambhanuprakash@gmail.com>
2026-01-26 22:03:54 +0530

    Fix Swagger server configuration

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -1,42 +1,46 @@
 import swaggerJSDoc from "swagger-jsdoc";
 
 const options = {
-                definition : {
-                openapi : '3.0.0',
-                components: {
-                securitySchemes: {
-                bearerAuth: {
-                type: "http",
-                scheme: "bearer",
-                bearerFormat: "JWT",
-           },
-       },
-    },
-                security: [
-     {
-                bearerAuth: [],
-     },
-  ],
+  definition: {
+    openapi: "3.0.0",
 
-        info:{
-            title: "Auth Microservice API",
-            version: "1.0.0",
-            description: "JWT Authentication & Authorization Microservice",
+    info: {
+      title: "Auth Microservice API",
+      version: "1.0.0",
+      description: "JWT Authentication & Authorization Microservice",
+    },
+
+    servers: [
+      {
+        url: "http://localhost:5000",
+        description: "Local Development Server",
+      },
+      {
+        url: "https://auth-microservice-5ki0.onrender.com",
+        description: "Render Production Server",
+      },
+    ],
+
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
         },
-         servers: [
-             {
-    url: "http://localhost:5000",
-    description: "Local server",
-  },
-  {
-    url: "https://auth-microservice-5ki0.onrender.com",
-    description: "Production server",
-  },
-     
-    ]
+      },
     },
-    apis: ["./src/routes/*.js"], // Swagger will read routes
-}
 
-const swaggerSpec = swaggerJSDoc(options)
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+
+  apis: ["./src/routes/*.js"], // Reads Swagger comments from routes
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
 export default swaggerSpec;
