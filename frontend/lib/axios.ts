e0b79f48d3fdfa41e44e3afe89fd3b492443d2b2
Bhanu Prakash <patnambhanuprakash@gmail.com>
2026-05-20 12:07:45 +0530

    fix : axios.ts

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-export const API_URL = '/api';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
 
 const api = axios.create({
   baseURL: API_URL,
